d558a0adb178a2b1bf6f67180dcebb322ed20fce
vutran <tranvu221097@gmail.com>
2023-11-21 22:21:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { generate } from "random-words";
+import { useState } from "react";
+import { SayButton } from 'react-say';
+import './App.css'
 
 function App() {
+  const [word, setWord] = useState("hello")
+  const generateNewWord = () => {
+    setWord(generate())
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="new-word">
+        <h1>{word}</h1>
+
+        <div className="play-button">
+          <SayButton
+            onClick={event => console.log(event)}
+            speak={word}
+
+          >
+            <svg className="icon-play" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+              <path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c7.6-4.2 16.8-4.1 24.3 .5l144 88c7.1 4.4 11.5 12.1 11.5 20.5s-4.4 16.1-11.5 20.5l-144 88c-7.4 4.5-16.7 4.7-24.3 .5s-12.3-12.2-12.3-20.9V168c0-8.7 4.7-16.7 12.3-20.9z" /></svg>
+          </SayButton>
+        </div>
+
+
+        <button onClick={generateNewWord} className="new-word-btn"><span>New Word</span></button>
+      </div>
+
     </div>
   );
 }
